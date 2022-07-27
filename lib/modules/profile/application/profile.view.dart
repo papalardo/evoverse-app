a700a42e7206c94091f32f1c1b45117a5e3143c6
Pablo Papalardo <pablopapalardo2@gmail.com>
2022-07-27 14:34:57 -0300

    feat: Update build info and show version app

--- lib/modules/profile/application/profile.view.dart
+++ lib/modules/profile/application/profile.view.dart
@@ -2,34 +2,37 @@ import "package:app/core/app.routes.dart";
 import "package:app/services/storage/istorage.service.dart";
 import "package:app/utils/functions.dart";
 import "package:app/utils/theme/app.palette.dart";
+import 'package:app/utils/widgets/loader/app-future-builder.widget.dart';
 import "package:flutter/material.dart";
 import "package:flutter_svg/svg.dart";
 import "package:get/get.dart";
 import "package:styled_widget/styled_widget.dart";
+import 'package:package_info_plus/package_info_plus.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   static const double kIconSize = 25;
   static const double kImageSize = 30;
-  
+
   Widget listTile({
     required String label,
     required Widget icon,
     required Function() onTap,
-  }) => ListTile(
-    shape: BeveledRectangleBorder(
-      side: const BorderSide(color: AppPalette.gray400, width: 1.5),
-      borderRadius: BorderRadius.circular(10)
-    ),
-    trailing: icon,
-    title: Text(label.toUpperCase())
-      .fontWeight(FontWeight.bold)
-      .fontSize(14)
-      .letterSpacing(3),
-    onTap: onTap,
-  );
-  
+  }) =>
+      ListTile(
+        shape: BeveledRectangleBorder(
+          side: const BorderSide(color: AppPalette.gray400, width: 1.5),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        trailing: icon,
+        title: Text(label.toUpperCase())
+            .fontWeight(FontWeight.bold)
+            .fontSize(14)
+            .letterSpacing(3),
+        onTap: onTap,
+      );
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -41,7 +44,8 @@ class ProfilePage extends StatelessWidget {
           //   onTap: () => Get.toNamed(AppRoutes.DONATE),
           // ),
           listTile(
-            icon: SvgPicture.asset(asset("svg/raffle.svg"),
+            icon: SvgPicture.asset(
+              asset("svg/raffle.svg"),
               width: kImageSize,
               height: kImageSize,
             ),
@@ -49,7 +53,8 @@ class ProfilePage extends StatelessWidget {
             onTap: () => Get.toNamed(AppRoutes.raffles),
           ),
           listTile(
-            icon: Image.asset(asset("images/training.png"),
+            icon: Image.asset(
+              asset("images/training.png"),
               width: kImageSize,
               height: kImageSize,
             ),
@@ -64,9 +69,14 @@ class ProfilePage extends StatelessWidget {
               Get.offAllNamed(AppRoutes.AUTH);
             },
           ),
-        ].toColumn(
-          separator: const SizedBox(height: 10)
-        ),
+          AppFutureBuilder(
+            future: PackageInfo.fromPlatform(),
+            done: (PackageInfo packageInfo) =>
+                Text("v${packageInfo.version}+${packageInfo.buildNumber}")
+                    .fontSize(10)
+                    .textColor(Colors.white.withOpacity(0.3)),
+          )
+        ].toColumn(separator: const SizedBox(height: 10)),
       ),
     );
   }
